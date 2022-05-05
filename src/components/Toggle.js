4d9daa6ad5843f96d7a59f2c90a67fe0ac73a3cf
annomao <omaoannkerubo@gmail.com>
2022-05-05 05:33:48 +0300

    add toggle on and off functionality

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React ,{ useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [isOn, setIsOn] = useState(false)
+
+  function toggleButton(){
+    setIsOn(()=> isOn = !isOn)
+  }
+  return <button onClick={toggleButton}>{isOn ? "ON": "OFF"}</button>;
 }
 
 export default Toggle;
